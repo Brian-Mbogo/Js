cd6333d33eaab1f22ccc543104a2fe14880f45cf
BrianMbogo <mbogo7070@gmail.com>
2025-12-02 13:13:17 +0300

    Initial commit

--- variables.js
+++ variables.js
@@ -1,3 +1,12 @@
-let name="Brian"
-console.log(Brian)
-alert(Brian)    
+
+let age=21;
+let name   =  "John Doe";
+
+console.log( "My current age is:",{age});
+
+
+console.log("My name is:",{name});
+
+
+age=21+10;
+console.log("My age after 10 years will be:",{age});
