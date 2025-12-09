19ef92184cf3cb9668fd001eedecc932850b9d9b
BrianMbogo <mbogo7070@gmail.com>
2025-12-09 11:32:06 +0300

    operators desriptively

--- Data-types.js
+++ Data-types.js
@@ -3,8 +3,12 @@ let userage = 25;
 console.log("My age is:", userage);
 
 // strings--->values with characters
-let username = "Alice Smith";
+let username = "Alice Smith"; //double quotes are used to define strings
 console.log("My name is:", username);
+let city = 'Los Angeles'; // single quotes are also used to define strings
+console.log("I live in:", city);    
+let country = `USA`; // backticks are used for template literals
+console.log("My country is:", country);       
 
 // boolen--->values with true or false
 let isStudent = true;
@@ -29,11 +33,16 @@ let uniqueId = Symbol("id");
 console.log("A unique identifier is:", uniqueId);
 
 // object--->collection of key-value pairs
-// let person = {
-    // firstName: "Bob",  }
+
     let person = { name: "Brian", age: 17 };
 console.log("Person object:", person);
 
 // array--->ordered collection of values
 let colors = ["red", "green", "blue"];
 console.log("Colors array:", colors);
+
+// null vs undefined
+let score;  // undefined -->variable declared but not assigned as in null establishes difference
+console.log("Score (undefined):", score);
+let level = null; // null--->intentionally no value compaRred to undefined    
+console.log("Level (null):", level);        
