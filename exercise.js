87fd59c0d4cfb08005ae884dd7aab987d7c196f9
BrianMbogo <mbogo7070@gmail.com>
2025-12-16 11:17:12 +0300

    Even and odd checker

--- exercise.js
+++ exercise.js
@@ -19,3 +19,16 @@ console.log("Sum of 5 and 3 is:", sum(5, 3));
      //  or arrow function version
 const sumArrow = (a, b) => a + b;
 console.log("Sum of 10 and 7 is:", sumArrow(10, 7));    
+
+
+// Even and ODD checker
+function isEven(num) {
+    return num % 2 === 0;  
+}   
+console.log("Is 4 even?", isEven(4));
+console.log("Is 7 even?", isEven(7));   
+
+//  or arrow function version
+const isEvenArrow = (num) => num % 2 === 0;
+console.log("Is 10 even?", isEvenArrow(10));
+console.log("Is 15 even?", isEvenArrow(15));    
